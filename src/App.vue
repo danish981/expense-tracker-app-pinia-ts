274fe97e6ec7163a19e6a57a21cff1d3101de327
danish <danishmehmood981@gmail.com>
2024-06-12 19:05:28 +0500

    added swal, and removed some unnecessary lines

--- src/App.vue
+++ src/App.vue
@@ -15,18 +15,4 @@ import IncomeExpenses from './components/IncomeExpenses.vue';
 import TransactionList from './components/TransactionList.vue';
 import AddTransaction from './components/AddTransaction.vue';
 
-function removeToastCloseButtons() {
-  const closeButtons = document.querySelectorAll('.Vue-Toastification__close-button');
-  closeButtons.forEach((button) => button.remove());
-}
-
-removeToastCloseButtons();
-
-
 </script>
-
-<style scoped>
-.Vue-Toastification__close-button {
-  display: none;
-}
-</style>
